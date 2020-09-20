f9bde26fd7f5bd955e714c98a9c6c012a9ac23de
krisvanlog <66163203+krisvanlog@users.noreply.github.com>
2020-09-20 22:23:38 +0200

    Update _PushToMT4Prod.bat

--- _PushToMT4Prod.bat
+++ _PushToMT4Prod.bat
@@ -4,11 +4,11 @@ rem Use when you need to publish all files to All Terminals
 @echo off
 setlocal enabledelayedexpansion
 
-set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\DataWriter"
-set DEST_DIR1="C:\Program Files (x86)\FxPro - Terminal1\MQL4\Experts\01_DataWriter"
-set DEST_DIR2="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts\01_DataWriter"
-set DEST_DIR3="C:\Program Files (x86)\FxPro - Terminal3\MQL4\Experts\01_DataWriter"
-set DEST_DIR4="C:\Program Files (x86)\FxPro - Terminal4\MQL4\Experts\01_DataWriter"
+set SOURCE_DIR="C:\Users\Kris\Documents\GitHub\LazyTrade\DataWriter"
+set DEST_DIR1="C:\Program Files (x86)\MT4_Terminal_1\MQL4\Experts\01_DataWriter"
+set DEST_DIR2="C:\Program Files (x86)\MT4_Terminal_2\MQL4\Experts\01_DataWriter"
+set DEST_DIR3="C:\Program Files (x86)\MT4_Terminal_3\MQL4\Experts\01_DataWriter"
+set DEST_DIR4="C:\Program Files (x86)\MT4_Terminal_4\MQL4\Experts\01_DataWriter"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR1% *.mq4
 ROBOCOPY %SOURCE_DIR% %DEST_DIR2% *.mq4
